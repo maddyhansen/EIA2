b11d53f2c4a85a61b1c0502bcfcfc2c081f54e8b
maddyhansen <madeleinehansen@hotmail.de>
2023-03-23 18:22:06 +0100

    Ende

--- 01_Aufgabe_random_POEM/script.js
+++ 01_Aufgabe_random_POEM/script.js
@@ -1,12 +1,30 @@
+"use strict";
 var RandomPoem;
 (function (RandomPoem) {
     let subject = ['Harry', 'Hermine', 'Ron', 'Hagrid', 'Snape', 'Dumbledore'];
     let praedicat = ['braut', 'liebt', 'studiert', 'hasst', 'zaubert', 'zerstört'];
     let object = ['Zaubertränke', 'den Grimm', 'Lupin', 'Hogwarts', 'die Karte des Rumtreibers', 'Dementoren'];
-    console.log(subject, praedicat, object);
-    let SubjectTotal = 5;
-    for (let index = 0; index <= SubjectTotal; index--) {
-        console.log("Hallo" + index);
+    //console.log(subject, praedicat, object)
+    // let subjectTotal: number = 6;
+    // console.log(subjectTotal)
+    function getVerse(_subject, _praedicat, _object) {
+        //console.log("Alohomora");
+        let words = "";
+        let result = Math.floor(Math.random() * _subject.length);
+        words = words + _subject[result] + " ";
+        _subject.splice(result, 1);
+        let results = Math.floor(Math.random() * _praedicat.length);
+        words = words + _praedicat[results] + " ";
+        _praedicat.splice(results, 1);
+        let resultsi = Math.floor(Math.random() * _object.length);
+        words = words + _object[resultsi] + " ";
+        _object.splice(resultsi, 1);
+        console.log(words + "");
+        return words;
+    }
+    for (let index = 6; index > 0; index--) {
+        // console.log("Word" + index)
+        getVerse(subject, praedicat, object);
     }
 })(RandomPoem || (RandomPoem = {}));
 //# sourceMappingURL=script.js.map
--- 01_Aufgabe_random_POEM/script.ts
+++ 01_Aufgabe_random_POEM/script.ts
@@ -1,14 +1,36 @@
 namespace RandomPoem {
 
-    let subject: string [] = ['Harry', 'Hermine', 'Ron', 'Hagrid', 'Snape', 'Dumbledore'];
-    let praedicat: string [] = ['braut', 'liebt', 'studiert', 'hasst', 'zaubert', 'zerstört'];
-    let object: string [] = ['Zaubertränke', 'den Grimm', 'Lupin', 'Hogwarts', 'die Karte des Rumtreibers', 'Dementoren'];
+    let subject: string[] = ['Harry', 'Hermine', 'Ron', 'Hagrid', 'Snape', 'Dumbledore'];
+    let praedicat: string[] = ['braut', 'liebt', 'studiert', 'hasst', 'zaubert', 'zerstört'];
+    let object: string[] = ['Zaubertränke', 'den Grimm', 'Lupin', 'Hogwarts', 'die Karte des Rumtreibers', 'Dementoren'];
+    //console.log(subject, praedicat, object)
+    // let subjectTotal: number = 6;
+    // console.log(subjectTotal)
 
-    console.log(subject, praedicat,object)
+    function getVerse(_subject: string[], _praedicat: string[], _object: string[]): string {
+        //console.log("Alohomora");
+
+        let words: string = "";
+        let result: number = Math.floor(Math.random() * _subject.length);
+        words = words + _subject[result] + " ";
+        _subject.splice(result, 1);
     
-    let SubjectTotal : number = 5
+        let results: number = Math.floor(Math.random() * _praedicat.length);
+        words = words + _praedicat[results] + " ";
+        _praedicat.splice(results, 1);
 
-    for (let index = 0; index <= SubjectTotal; index--) {
-        console.log ("Hallo" + index)
+        let resultsi: number = Math.floor(Math.random() * _object.length);
+        words = words + _object[resultsi]+ " ";
+        _object.splice(resultsi, 1);
+        console.log(words + "");
+        return words;
     }
+ 
+
+
+    for (let index = 6; index > 0; index--) {
+        // console.log("Word" + index)
+        getVerse(subject , praedicat, object);
+    }
+
 }
