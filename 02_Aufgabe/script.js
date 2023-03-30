799ea872f58ce78d70e353e09d48719db19dc268
maddyhansen <madeleinehansen@hotmail.de>
2023-03-30 17:09:48 +0200

    hello

--- 02_Aufgabe/script.js
+++ 02_Aufgabe/script.js
@@ -1,7 +1,6 @@
 "use strict";
 var eventuebung;
 (function (eventuebung) {
-    debugger;
     console.log("Hallo Start");
     window.addEventListener('load', handleLoad);
     function handleLoad() {
@@ -10,17 +9,21 @@ var eventuebung;
         document.addEventListener('keyup', logInfo);
         document.body.addEventListener('click', logInfo);
         document.body.addEventListener('keyup', logInfo);
-        document.getElementById('#div0').addEventListener('click', logInfo);
-        document.getElementById('#div0').addEventListener('keyup', logInfo);
-        document.getElementById('#div1').addEventListener('click', logInfo);
-        document.getElementById('#div1').addEventListener('keyup', logInfo);
+        document.getElementById('div0').addEventListener('click', logInfo);
+        document.getElementById('div0').addEventListener('keyup', logInfo);
+        document.getElementById('div1').addEventListener('click', logInfo);
+        document.getElementById('div1').addEventListener('keyup', logInfo);
     }
     function setInfoBox(_event) {
         let positionx = _event.clientX;
         let positiony = _event.clientY;
         let span = document.createElement("span1");
-        span.innerHTML = positionx + "  " + positiony;
+        span.innerHTML = "span";
+        span.className = "span";
+        document.querySelector("#div0").appendChild(span);
+        span.innerText = "Mouseposition: x : " + positionx + ", Y: " + positiony + _event.target;
         span.setAttribute('style', 'right: positionx + value + 10px');
+        //span.setAttribute('style','right: positiony + value + 10px');
     }
     function logInfo(_event) {
         console.log(_event.target, _event.currentTarget, _event.type, _event);
--- 02_Aufgabe/script.ts
+++ 02_Aufgabe/script.ts
@@ -1,29 +1,34 @@
 namespace eventuebung {
-  debugger;
   console.log("Hallo Start")
   window.addEventListener('load', handleLoad)
 
-  function handleLoad():void {
+  function handleLoad(): void {
     document.addEventListener('mousemove', setInfoBox);
     document.addEventListener('click', logInfo);
     document.addEventListener('keyup', logInfo);
     document.body.addEventListener('click', logInfo);
     document.body.addEventListener('keyup', logInfo);
-    document.getElementById('#div0')!.addEventListener('click', logInfo);
-    document.getElementById('#div0')!.addEventListener('keyup', logInfo);
-    document.getElementById('#div1')!.addEventListener('click', logInfo);
-    document.getElementById('#div1')!.addEventListener('keyup', logInfo);
+    document.getElementById('div0')!.addEventListener('click', logInfo);
+    document.getElementById('div0')!.addEventListener('keyup', logInfo);
+    document.getElementById('div1')!.addEventListener('click', logInfo);
+    document.getElementById('div1')!.addEventListener('keyup', logInfo);
   }
 
   function setInfoBox(_event: MouseEvent): void {
     let positionx: number = _event.clientX;
     let positiony: number = _event.clientY;
-    let span : HTMLElement = document.createElement("span1");
-    span.innerHTML= positionx + "  " + positiony;
-    span.setAttribute('style','right: positionx + value + 10px');
+
+    let span: HTMLElement = document.createElement("span1");
+    span.innerHTML = "span";
+    span.className = "span";
+    document.querySelector("#div0")!.appendChild(span)
+    span.innerText = "Mouseposition: x : " + positionx + ", Y: " + positiony + _event.target;
+    span.setAttribute('style', 'right: positionx + value + 10px');
+    //span.setAttribute('style','right: positiony + value + 10px');
   }
 
   function logInfo(_event: Event): void {
     console.log(_event.target, _event.currentTarget, _event.type, _event)
   }
+
 }
