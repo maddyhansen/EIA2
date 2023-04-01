30de72690cd41bfbcf00b7f2cb68a85933eec7f9
maddyhansen <madeleinehansen@hotmail.de>
2023-04-01 17:48:30 +0200

    hub

--- 02_Aufgabe/script.js
+++ 02_Aufgabe/script.js
@@ -8,10 +8,10 @@ var eventuebung;
     span.className = "span";
     document.querySelector("#div0").appendChild(span);
     function btn() {
-        console.log("hi ich funktioniere");
+        console.log("I was clicked!", customEvent);
     }
     function handleLoad() {
-        document.querySelector("#button")?.addEventListener('click', btn);
+        document.querySelector("button").addEventListener('click', btn);
         document.addEventListener('mousemove', setInfoBox);
         document.addEventListener('click', logInfo);
         document.addEventListener('keyup', logInfo);
@@ -25,18 +25,19 @@ var eventuebung;
     function setInfoBox(_event) {
         let positionx = _event.clientX;
         let positiony = _event.clientY;
-        span.style.left = positionx + "px";
-        span.style.top = positiony + "px";
+        span.style.left = positionx + 10 + "px";
+        span.style.top = positiony + 10 + "px";
         span.innerText = "Mouseposition: x : " + positionx + ", Y: " + positiony + _event.target;
     }
     function logInfo(_event) {
         console.log(_event.target, _event.currentTarget, _event.type, _event);
     }
-    //function customEvent(_event: MouseEvent): void {
-    //const customEvent = new CustomEvent('BubblemeupBob', {
-    //  bubbles: true, 
-    // detail:{bubble: setInfoBox}});
-    // document.querySelector('button')!.dispatchEvent(customEvent)
-    // }
+    function customEvent(_event) {
+        const customEvent = new CustomEvent('BubblemeupBob', {
+            bubbles: true,
+            detail: { bubble: setInfoBox }
+        });
+        document.querySelector('button').dispatchEvent(customEvent);
+    }
 })(eventuebung || (eventuebung = {}));
 //# sourceMappingURL=script.js.map
--- 02_Aufgabe/script.ts
+++ 02_Aufgabe/script.ts
@@ -8,12 +8,13 @@ namespace eventuebung {
   span.className = "span";
   document.querySelector("#div0")!.appendChild(span);
 
+
   function btn(): void {
-    console.log("hi ich funktioniere")
+    console.log("I was clicked!", customEvent)
   }
 
   function handleLoad(): void {
-    document.querySelector("#button")?.addEventListener('click',btn);
+    document.querySelector("button")!.addEventListener('click', btn);
     document.addEventListener('mousemove', setInfoBox);
     document.addEventListener('click', logInfo);
     document.addEventListener('keyup', logInfo);
@@ -28,8 +29,10 @@ namespace eventuebung {
   function setInfoBox(_event: MouseEvent): void {
     let positionx: number = _event.clientX;
     let positiony: number = _event.clientY;
-    span.style.left = positionx + "px";
-    span.style.top = positiony + "px";
+
+   span.style.left = positionx + 10 + "px";
+   span.style.top = positiony + 10 + "px";
+
     span.innerText = "Mouseposition: x : " + positionx + ", Y: " + positiony + _event.target;
   }
 
@@ -37,10 +40,10 @@ namespace eventuebung {
     console.log(_event.target, _event.currentTarget, _event.type, _event);
   }
 
-  //function customEvent(_event: MouseEvent): void {
-    //const customEvent = new CustomEvent('BubblemeupBob', {
-    //  bubbles: true, 
-     // detail:{bubble: setInfoBox}});
-     // document.querySelector('button')!.dispatchEvent(customEvent)
- // }
+  function customEvent(_event: MouseEvent): void {
+  const customEvent = new CustomEvent('BubblemeupBob', {
+  bubbles: true, 
+  detail:{bubble: setInfoBox}});
+  document.querySelector('button')!.dispatchEvent(customEvent)
+  }
 }
