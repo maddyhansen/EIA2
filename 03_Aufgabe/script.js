69465f449a6846f434e912eb2f3a2b9863fa9ac8
maddyhansen <madeleinehansen@hotmail.de>
2023-04-07 13:44:15 +0200

    Endabgabe

--- 03_Aufgabe/script.js
+++ 03_Aufgabe/script.js
@@ -2,5 +2,20 @@
 var todoapp;
 (function (todoapp) {
     console.log("Start");
+    window.addEventListener('load', handleLoad);
+    function handleLoad() {
+        document.getElementById("newtodo").addEventListener('click', newTodo);
+        document.getElementById("edit").addEventListener('click', editForm);
+        document.getElementById("add").addEventListener('click', addToDO);
+    }
+    function newTodo() {
+        console.log("Hi, please fill the fields out for your ToDo");
+    }
+    function editForm() {
+        console.log("Hi, I am editing my todo");
+    }
+    function addToDO() {
+        console.log("Hi, I am a ToDo added to the list!");
+    }
 })(todoapp || (todoapp = {}));
 //# sourceMappingURL=script.js.map
--- 03_Aufgabe/script.ts
+++ 03_Aufgabe/script.ts
@@ -1,5 +1,24 @@
 namespace todoapp {
 console.log("Start")
 
+window.addEventListener('load', handleLoad);
 
+function handleLoad(): void {
+    document.getElementById("newtodo")!.addEventListener('click', newTodo);
+    document.getElementById("edit")!.addEventListener('click', editForm);
+    document.getElementById("add")!.addEventListener('click', addToDO);
+  }
+
+
+function newTodo(){
+    console.log("Hi, please fill the fields out for your ToDo")
+}
+
+function editForm(){
+    console.log("Hi, I am editing my todo")
+}
+
+function addToDO() {
+    console.log("Hi, I am a ToDo added to the list!")
+}
 }
