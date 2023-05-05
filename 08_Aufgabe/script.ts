68c67108d9a914fba6d2125a420961ab5cfb7770
maddyhansen <madeleinehansen@hotmail.de>
2023-05-05 17:35:29 +0200

    ujuhuhu

--- 08_Aufgabe/script.ts
+++ 08_Aufgabe/script.ts
@@ -2,6 +2,11 @@ namespace Canvas {
 
     console.log("Its me, hi I am the problem its me!")
 
+    interface Vector {
+        x: number; 
+        y: number; 
+    }
+
     window.addEventListener('load', handleload);
     let crc2: CanvasRenderingContext2D;
     let canvas: HTMLCanvasElement;
@@ -13,8 +18,10 @@ namespace Canvas {
         crc2 = <CanvasRenderingContext2D>canvas!.getContext("2d")
         console.log("Iamhereeee");
         changeColor();
-       // randomPattern();
-        randomDots();
+        // randomPattern();
+        // randomDots();
+        //drawLine();
+        drawParticle({x: 500, y:125}, {x: 1000, y: -500});
 
     }
 
@@ -30,88 +37,59 @@ namespace Canvas {
         document.body.style.backgroundColor = randomColor;
         crc2.fillStyle = "randomColor";
         console.log(randomColor);
-        crc2.fillRect(0, 0, canvas.width, canvas.height);
+        //crc2.fillRect(0, 0, canvas.width, canvas.height);
     }
 
-   /* function randomPattern() {
-        let rgb = []
-        for (let index = 0; index < 1; index++)
-            rgb.push(Math.floor(Math.random() * 256))
-        crc2.fillStyle = "#FF111111";
-        crc2.fillRect(0, 0, crc2.canvas.width, crc2.canvas.height);
-        crc2.beginPath();
-        crc2.arc(100, 100, 20, 0, 1.5 * Math.PI);
-        crc2.closePath();
-        crc2.stroke();
+   /* function getRandomNumber(_max: number, _min: number = 0): number {
+        return Math.floor(Math.random() * _max) + _min;
     }*/
 
-    function randomDots() {
-        canvas.height = canvas.width;
-        crc2.transform(1, 0, 0, -1, 0, canvas.height)
+    
+        function drawParticle(_position: Vector, _size: Vector):void{
+            console.log("T2");
+            let nParticles: number = 10;
+            let particle: Path2D = new Path2D();
+              let radiusParticle: number = 8; 
+          let gradient: CanvasGradient = crc2.createRadialGradient(0,0,0,0,0,radiusParticle);
+        
+            particle.arc(0,0,radiusParticle,0,2*Math.PI);
+            gradient.addColorStop(0,"lightblue");
+            gradient.addColorStop(1,"blue"); 
+            changeColor();
+            crc2.fillStyle = gradient;
+        
+            crc2.save();  
+            crc2.translate(_position.x, _position.y);
+            crc2.restore(); 
+        
+            for (let drawn: number = 0; drawn < nParticles; drawn++){
+            let x: number = (Math.random() - 0.5)* _size.x; 
+            let y: number = - (Math.random() * _size.y);
+            crc2.save();
+            crc2.translate(x,y);    
+            crc2.fill(particle);
+            crc2.restore(); 
+        
+            }
 
-        const xArray = [50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150];
-        const yArray = [7, 8, 8, 9, 9, 9, 10, 11, 14, 14, 15];
 
-        crc2.fillStyle = "white";
-        for (let i = 0; i < xArray.length - 1; i++) {
-            let x = xArray[i] * 400 / 150;
-            let y = yArray[i] * 400 / 15;
-            crc2.beginPath();
-            crc2.ellipse(x, y, 3, 3, 0, 0, Math.PI * 2);
-            crc2.fill();
-        }
+        /*function randomDots() 
+            const intensity = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, "A", "B", "C", "D", "E", "F"];
+            //canvas.height = canvas.width;
+            //crc2.transform(1, 0, 0, -1, 0, canvas.height)
+    
+            let xArray = [];
+            const yArray = [7, 8, 8, 9, 9, 9, 10, 11, 14, 14, 15];
+    
+            crc2.fillStyle = "white";
+            for (let i = 0; i < xArray.length - 1; i++) {
+                let x = xArray[i] * 400 / 150;
+                let y = yArray[i] * 400 / 15;
+                crc2.beginPath();
+                crc2.ellipse(x, y, 3, 3, 0, 0, Math.PI * 2);
+                crc2.fill();
+            }*/
+
+
     }
-
-
-
-    /* crc2.fillStyle = "#FF111111";
-           crc2.fillRect(0, 0, crc2.canvas.width, crc2.canvas.height);
-           crc2.beginPath();
-           crc2.arc(100, 100, 20, 0, 1.5 * Math.PI);
-           crc2.closePath();
-           crc2.stroke();
-           let path: Path2D = new Path2D();
-           path.arc(60, 60, 50, 0, 2 * Math.PI);
-           crc2.stroke(path);
-           crc2.beginPath();
-           crc2.moveTo(2.1, 1);
-           crc2.lineTo(2.1, 10);
-           crc2.moveTo(4.5, 1);
-           crc2.lineTo(4.5, 10);
-           crc2.moveTo(7.5, 1);
-           crc2.lineTo(10.5, 10);
-           crc2.stroke();
-           let gradient: CanvasGradient = crc2.createLinearGradient(0, 0, 0, 100);
-    
-    
-           let pattern: CanvasRenderingContext2D = document.createElement('canvas').getContext('2d');
-           pattern.canvas.width = 40;
-           pattern.canvas.height = 20;
-    
-           pattern.fillStyle = '#fec';
-           pattern.fillRect(0, 0, pattern.canvas.width, pattern.canvas.height);
-           pattern.moveTo(0, 10);
-           pattern.lineTo(10, 10);
-           pattern.lineTo(20, 0);
-           pattern.lineTo(30, 0);
-           pattern.lineTo(40, 10);
-           pattern.lineTo(30, 20);
-           pattern.lineTo(20, 20);
-           pattern.lineTo(10, 10);
-           pattern.stroke();
-    
-           crc2.fillStyle = crc2.createPattern(pattern.canvas, 'repeat');
-           crc2.fillRect(0, 0, canvas.width, canvas.height);*/
-    //  Array farben 
-    /* array pattern 
-     array strichen
-     array kreisen 
-    
-    
-     create something 
-     math random 
-    
-     canvas */
-
-
 }
