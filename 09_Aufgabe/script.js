aa406bda522ee55417d8f616ba8f45ebbedb5c20
maddyhansen <madeleinehansen@hotmail.de>
2023-05-19 19:05:40 +0200

    hhh

--- 09_Aufgabe/script.js
+++ 09_Aufgabe/script.js
@@ -3,10 +3,9 @@ var OldMcDonald;
 (function (OldMcDonald) {
     /*normal script*/
     window.addEventListener("load", start);
-    let canvas;
     function start(_event) {
         let canvas = document.querySelector("canvas");
-        crc2: CanvasRenderingContext2D;
+        OldMcDonald.crc2 = canvas.getContext("2d");
         let donkey = new OldMcDonald.Animal(70, "grey");
         console.log(donkey);
         donkey.draw();
--- 09_Aufgabe/script.ts
+++ 09_Aufgabe/script.ts
@@ -3,11 +3,11 @@ namespace OldMcDonald {
   /*normal script*/
   window.addEventListener("load", start);
   export let crc2: CanvasRenderingContext2D;
-  let canvas: HTMLCanvasElement;
+  export let canvas: HTMLCanvasElement;
 
   function start(_event: Event): void {
     let canvas: HTMLCanvasElement = document.querySelector("canvas")!;
-    crc2: CanvasRenderingContext2D;
+    crc2 = <CanvasRenderingContext2D>canvas.getContext("2d");
 
     let donkey : Animal = new Animal(70, "grey")
     console.log(donkey)
