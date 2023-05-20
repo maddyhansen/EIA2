9c10678eec422f7cc395ce23b4b22a56e52da32f
maddyhansen <madeleinehansen@hotmail.de>
2023-05-20 13:27:38 +0200

    Weiter

--- 09_Aufgabe/script.js
+++ 09_Aufgabe/script.js
@@ -26,6 +26,10 @@ var OldMcDonald;
     }
     function newDay() {
         console.log("Hallo0");
+        donkey();
+    }
+    function donkey() {
+        console.log("Hallo0");
         let donkey = new OldMcDonald.Animal("Donkey", "EHHHHHAHHHHH", "OLD MC DONALD HAD A FARM!", "HEU", 10);
         console.log(donkey);
         donkey.name = "_name";
@@ -33,16 +37,49 @@ var OldMcDonald;
         donkey.sing = "_sing";
         donkey.eat = "_eat";
         donkey.food = 10;
-        //donkey.draw();
         donkey.soundAnimal();
         console.log(donkey.soundAnimal());
         donkey.singingSong();
         donkey.eatFood(10);
-        //donkey.drawBody();
-        //donkey.drawHead();
-        //donkey.drawEyes();
-        //donkey.drawNose();
-        //donkey.drawEars();
+        OldMcDonald.crc2.fillStyle = "#8a8a8a";
+        OldMcDonald.crc2.fillRect(847, 250, 55, 50);
+        OldMcDonald.crc2.fillStyle = "#9c9b9a";
+        OldMcDonald.crc2.fillRect(837, 195, 75, 70);
+        OldMcDonald.crc2.fillStyle = "black";
+        OldMcDonald.crc2.fillRect(850, 215, 12, 20);
+        OldMcDonald.crc2.fillStyle = "white";
+        OldMcDonald.crc2.fillRect(853, 218, 4, 4);
+        OldMcDonald.crc2.fillStyle = "black";
+        OldMcDonald.crc2.fillRect(885, 215, 12, 20);
+        OldMcDonald.crc2.fillStyle = "white";
+        OldMcDonald.crc2.fillRect(888, 218, 4, 4);
+        OldMcDonald.crc2.beginPath();
+        OldMcDonald.crc2.ellipse(873, 248, 12, 12, 0, 0, Math.PI * 2);
+        OldMcDonald.crc2.fillStyle = "#8a8a8a";
+        OldMcDonald.crc2.fill();
+        OldMcDonald.crc2.closePath();
+        OldMcDonald.crc2.beginPath();
+        OldMcDonald.crc2.ellipse(867, 248, 3, 3, 0, 0, Math.PI * 2);
+        OldMcDonald.crc2.fillStyle = "#9c9b9a";
+        OldMcDonald.crc2.fill();
+        OldMcDonald.crc2.closePath();
+        OldMcDonald.crc2.beginPath();
+        OldMcDonald.crc2.ellipse(878, 248, 3, 3, 0, 0, Math.PI * 2);
+        OldMcDonald.crc2.fillStyle = "#9c9b9a";
+        OldMcDonald.crc2.fill();
+        OldMcDonald.crc2.closePath();
+        OldMcDonald.crc2.fillStyle = "#9c9b9a";
+        OldMcDonald.crc2.beginPath();
+        OldMcDonald.crc2.moveTo(830, 160);
+        OldMcDonald.crc2.lineTo(837, 195);
+        OldMcDonald.crc2.lineTo(860, 195);
+        OldMcDonald.crc2.fill();
+        OldMcDonald.crc2.fillStyle = "#9c9b9a";
+        OldMcDonald.crc2.beginPath();
+        OldMcDonald.crc2.moveTo(920, 160);
+        OldMcDonald.crc2.lineTo(912, 195);
+        OldMcDonald.crc2.lineTo(890, 195);
+        OldMcDonald.crc2.fill();
     }
     function singing() {
         console.log("Hallo1");
--- 09_Aufgabe/script.ts
+++ 09_Aufgabe/script.ts
@@ -31,7 +31,12 @@ namespace OldMcDonald {
     crc2.fillRect(0, 290, crc2.canvas.width, 250);
   }
 
-  function newDay(): void{
+  function newDay(): void {
+    console.log("Hallo0")
+    donkey();
+  }
+
+  function donkey() {
     console.log("Hallo0")
     let donkey: Animal = new Animal("Donkey", "EHHHHHAHHHHH", "OLD MC DONALD HAD A FARM!", "HEU", 10)
     console.log(donkey)
@@ -40,24 +45,68 @@ namespace OldMcDonald {
     donkey.sing = "_sing";
     donkey.eat = "_eat";
     donkey.food = 10;
-    //donkey.draw();
     donkey.soundAnimal();
     console.log(donkey.soundAnimal())
 
     donkey.singingSong();
-    donkey.eatFood(10)
-    //donkey.drawBody();
-    //donkey.drawHead();
-    //donkey.drawEyes();
-    //donkey.drawNose();
-    //donkey.drawEars();
+    donkey.eatFood(10);
+
+    crc2.fillStyle = "#8a8a8a";
+    crc2.fillRect(847, 250, 55, 50);
+
+    crc2.fillStyle = "#9c9b9a";
+    crc2.fillRect(837, 195, 75, 70);
+
+    crc2.fillStyle = "black";
+    crc2.fillRect(850, 215, 12, 20);
+    crc2.fillStyle = "white";
+    crc2.fillRect(853, 218, 4, 4);
+
+
+    crc2.fillStyle = "black";
+    crc2.fillRect(885, 215, 12, 20);
+    crc2.fillStyle = "white";
+    crc2.fillRect(888, 218, 4, 4);
+
+    crc2.beginPath();
+    crc2.ellipse(873, 248, 12, 12, 0, 0, Math.PI * 2);
+    crc2.fillStyle = "#8a8a8a";
+    crc2.fill();
+    crc2.closePath();
+
+    crc2.beginPath();
+    crc2.ellipse(867, 248, 3, 3, 0, 0, Math.PI * 2);
+    crc2.fillStyle = "#9c9b9a";
+    crc2.fill();
+    crc2.closePath();
+
+    crc2.beginPath();
+    crc2.ellipse(878, 248, 3, 3, 0, 0, Math.PI * 2);
+    crc2.fillStyle = "#9c9b9a";
+    crc2.fill();
+    crc2.closePath();
+
+    crc2.fillStyle = "#9c9b9a";
+    crc2.beginPath();
+    crc2.moveTo(830, 160);
+    crc2.lineTo(837, 195);
+    crc2.lineTo(860, 195);
+    crc2.fill();
+
+    crc2.fillStyle = "#9c9b9a";
+    crc2.beginPath();
+    crc2.moveTo(920, 160);
+    crc2.lineTo(912, 195);
+    crc2.lineTo(890, 195);
+    crc2.fill();
+
   }
 
-  function singing(){
+  function singing() {
     console.log("Hallo1")
   }
 
-  function food(){
+  function food() {
     console.log("Hallo2")
   }
 
